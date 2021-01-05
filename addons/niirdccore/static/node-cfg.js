259507a37507b66d822b7a33c3e4c658e118a22a
ivis-sasaki <junko.sasaki@ivis.co.jp>
2021-01-05 11:47:01 +0900

    niirdccoreアドオンのEnable/Disableを画面上で変更できるように修正

---
+++ addons/niirdccore/static/node-cfg.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var $ = require('jquery');
+var m = require('mithril');
+var ko = require('knockout');
+var Raven = require('raven-js');
+var osfHelpers = require('js/osfHelpers');
+var ChangeMessageMixin = require('js/changeMessage');
+var SaveManager = require('js/saveManager');
+
+var SHORT_NAME = 'niirdccore';
+var logPrefix = `[${SHORT_NAME}] `;
+
+
+function NodeSettings() {
+  var self = this;
+  self.baseUrl = window.contextVars.node.urls.api + SHORT_NAME + '/';
+
+  ChangeMessageMixin.call(self);
+
+  self.loadConfig = function() {
+    var url = self.baseUrl + 'settings';
+    console.log(logPrefix, 'loading: ', url);
+
+var settings = new NodeSettings();
+osfHelpers.applyBindings(settings, `#${SHORT_NAME}Scope`);
+settings.loadConfig();
