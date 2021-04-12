fcd7790edbdc63f60faf5dfefd1ab67c0c1925eb
ivis-kuwata <jumpei.kuwata@ivis.co.jp>
2021-04-11 17:54:23 -0700

    fix: add test case

--- addons/niirdccore/tests/test_views.py
+++ addons/niirdccore/tests/test_views.py
@@ -17,20 +17,34 @@ from addons.base.tests.views import (
     OAuthAddonAuthViewsTestCaseMixin, OAuthAddonConfigViewsTestCaseMixin
 )
 from tests.base import OsfTestCase, get_default_metaschema
-from osf_tests.factories import ProjectFactory, UserFactory, InstitutionFactory
+from osf_tests.factories import ProjectFactory, AuthUserFactory, InstitutionFactory
 from admin.rdm_addons.utils import get_rdm_addon_option
 
+import website
 from website.util import api_url_for
 from addons.niirdccore.tests.utils import NiirdccoreAddonTestCase
 from addons.niirdccore import apps, models, views, SHORT_NAME
 
-# pytestmark = pytest.mark.django_db
+# requests.getのmock
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def raise_for_status(self):
+            return self.status_code
+
+        def json(self):
+            return self.json_data
+
+    # if args[0] == 'url_valid':
+    #     return MockResponse({'dmp': 'dummy_value'}, 200)
+
+    return MockResponse({'dmp': 'dummy_value'}, 200)
+
 
 class TestNiirdccoreViews(NiirdccoreAddonTestCase,  OsfTestCase):
-
-    # def setUp(self):
-    #     super(TestNiirdccoreViews, self).setUp()
-    #     self.set_node_settings(self.node_settings)
 
     # @mock.patch.object(models.AddonList, 'set_node_id')
     # @mock.patch.object(models.AddonList, 'set_addon_id')
@@ -39,27 +53,17 @@ class TestNiirdccoreViews(NiirdccoreAddonTestCase,  OsfTestCase):
     # @mock.patch.object(apps.AddonAppConfig, 'node_settings', NodeFactory)
     # def test_niirdccore_apply_dmp_subscribe(self):
 
-        # self.node_settings.set_dmp_id('78c0f674a39962f2a70f7e9a9d783805')
-        # self.node_settings.save()
-        # url = self.project.api_url_for('{}_apply_dmp_subscribe'.format(SHORT_NAME))
-        # res = self.app.get(
-        #     url,
-        #     auth=self.user.auth,
-        #     node=self.nodesettings,
-        #     addon_id='test',
-        #     callback='test.callback'
-        # )
-        # assertIsNone(subscript)
-        # subscript = views.niirdccore_apply_dmp_subscribe(
-        #     auth=self.user.auth,
-        #     # auth=Auth(user=mock_user),
-        #     # node=mock_node,
-        #     node=self.project,
-        #     addon_id='test',
-        #     callback='test.callback')
-
-
-    #! mock sample (from: https://qiita.com/East-Da/items/211d06359a7573116eb7#%E3%83%A2%E3%82%B8%E3%83%A5%E3%83%BC%E3%83%AB%E3%81%AE%E9%96%A2%E6%95%B0%E3%82%92mock%E3%81%99%E3%82%8B)
+    #     self.node_settings.set_dmp_id('78c0f674a39962f2a70f7e9a9d783805')
+    #     self.node_settings.save()
+    #     user = AuthUserFactory()
+    #     user.auth = (user.username, 'queenfan86')
+    #     subscript = views.niirdccore_apply_dmp_subscribe(
+    #         # auth=Auth(user=mock_user),
+    #         # node=mock_node,
+    #         node=self.project,
+    #         auth=user.auth,
+    #         addon_id='test',
+    #         callback='test.callback')
 
     def test_niirdccore_set_config(self):
         # jupyterhub none
@@ -115,20 +119,18 @@ class TestNiirdccoreViews(NiirdccoreAddonTestCase,  OsfTestCase):
             )
             assert_equals(res.status_code, 400)
 
-    # @mock.patch('requests.get', mock.Mock(return_value=200))
-    # @mock.path('requests.Response.raise_for_status', mock.Mock(return_value=requests.Response()))
-    # def test_niirdccore_get_dmp_info(self):
-        # status_code = 200
-        # response_mock = mock.Mock()
-        # response_mock.status_code = status_code
-        # mock.patch.object(requests, "get", return_value=response_mock)
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_niirdccore_get_dmp_info(self, mock_get):
+        expected_res = {'data': {'id': self.project._id, 'type': 'dmp-status', 'attributes': 'dummy_value'}}
 
-        # self.node_settings.set_dmp_id('78c0f674a39962f2a70f7e9a9d783805')
-        # self.node_settings.set_dmr_api_key('dummy_key')
+        self.node_settings.set_dmp_id('dummy_id')
+        self.node_settings.set_dmr_api_key('dummy_key')
 
-        # url = self.project.api_url_for('{}_get_dmp_info'.format(SHORT_NAME))
-        # res = self.app.get(url, auth=self.user.auth)
-        # assert_equals(res.status_code, 200)
+        url = self.project.api_url_for('{}_get_dmp_info'.format(SHORT_NAME))
+        res = self.app.get(url, auth=self.user.auth)
+        assert_equals(res.status_code, 200)
+        assert_equals(res.json, expected_res)
+
 
     def test_niirdccore_get_dmp_info_denied(self):
         with pytest.raises(AppError):
@@ -136,51 +138,54 @@ class TestNiirdccoreViews(NiirdccoreAddonTestCase,  OsfTestCase):
             res = self.app.get(url, auth=self.user.auth)
             assert_equals(res.status_code, 410)
 
-    # def test_niirdccore_update_dmp_info(self):
-    #     self.node_settings.set_dmp_id('78c0f674a39962f2a70f7e9a9d783805')
-    #     self.node_settings.set_dmr_api_key('dummy_key')
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_niirdccore_update_dmp_info(self, mock_get):
+        self.node_settings.set_dmp_id('dummy_id')
+        self.node_settings.set_dmr_api_key('dummy_key')
 
-    #     url = self.project.api_url_for('{}_update_dmp_info'.format(SHORT_NAME))
+        url = self.project.api_url_for('{}_update_dmp_info'.format(SHORT_NAME))
 
-    #     # create dataset
-    #     res = self.app.patch_json(
-    #         url,
-    #         {
-    #             'data': {
-    #                 'attributes': {
-    #                     'dataset': [
-    #                         {
-    #                             'datasetIsNew': 'true',
-    #                             'datasetId': {
-    #                                 'identifier': 'dummy_dataset_id'
-    #                             }
-    #                         }
-    #                     ]
-    #                 }
-    #             }
-    #         },
-    #         auth=self.user.auth,
-    #     )
-    #     assert_equals(res.status_code, 200)
+        # create dataset
+        res = self.app.patch_json(
+            url,
+            {
+                'data': {
+                    'attributes': {
+                        'dataset': [
+                            {
+                                'datasetIsNew': 'true',
+                                'datasetId': {
+                                    'identifier': 'dummy_dataset_id'
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            auth=self.user.auth,
+        )
+        assert_equals(res.status_code, 200)
 
-    #     res = self.app.patch_json(
-    #         url,
-    #         {
-    #             'data': {
-    #                 'attributes': {
-    #                     'dataset': [
-    #                         {
-    #                             'datasetIsNew': 'false',
-    #                             'datasetId': {
-    #                                 'identifier': 'dummy_dataset_id'
-    #                             }
-    #                         }
-    #                     ]
-    #                 }
-    #             }
-    #         },
-    #         auth=self.user.auth,
-    #     )
+        # update dataset
+        res = self.app.patch_json(
+            url,
+            {
+                'data': {
+                    'attributes': {
+                        'dataset': [
+                            {
+                                'datasetIsNew': 'false',
+                                'datasetId': {
+                                    'identifier': 'dummy_dataset_id'
+                                }
+                            }
+                        ]
+                    }
+                }
+            },
+            auth=self.user.auth,
+        )
+        assert_equals(res.status_code, 200)
 
     def test_niirdccore_update_dmp_info_denied(self):
         with pytest.raises(AppError):
